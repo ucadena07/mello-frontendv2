a1ccf780df27d3da5a7a985610a45ab29e0f3b27
Ulises Cadena <ucadena07@gmail.com>
2020-03-25 09:39:27 -0500

    Modeling the board

---
+++ assets/js/board.js
@@ -0,0 +1,36 @@
+class Card {
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+class List {
+  constructor(title) {
+    this.title = title;
+    this.cards = [];
+  }
+
+  addCard(text) {
+    var card = new Card(text);
+    this.cards.push(card);
+  }
+}
+
+class Board {
+  constructor() {
+    this.lists = [];
+  }
+
+  addList(text) {
+    var list = new List(text);
+    this.lists.push(list);
+  }
+}
+
+
+
+var list = new List("My first list");
+list.addCard("My first card");
+
+console.log(list);
+console.log(List.prototype);
